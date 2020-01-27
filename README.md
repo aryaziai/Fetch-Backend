a1c27298451c8a31b29be6e6349ea1c43cd52b03
aryaziai <arya721@gmail.com>
2020-01-27 14:44:16 -0800

    restarted but with rails flag

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # README
 
-FETCH Backend.
+This README would normally document whatever steps are necessary to get the
+application up and running.
 
 Things you may want to cover:
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "WeTranslate_Backend_production"
+  # config.active_job.queue_name_prefix = "Fetch_Backend_production"
 
   config.action_mailer.perform_caching = false
 
